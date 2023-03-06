b8ce27370d79c88d0b5390d019885e91624d29e3
chensq <chensq0120@gmail.com>
2023-03-06 20:24:42 +0800

    data i/o and framework of training

---
+++ main_train_sample.py
@@ -0,0 +1,155 @@
+import os.path
+import math
+import argparse
+import time
+import random
+import numpy as np
+from collections import OrderedDict
+import logging
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
+import torch
+
+from utils import utils_logger
+from utils import utils_image as util
+from utils import utils_option as option
+from utils.utils_dist import init_dist, get_dist_info
+
+from data.select_dataset import define_Dataset
+
+def main(json_path='options/option.json'):
+
+    '''
+    # ----------------------------------------
+    # Step--1 (prepare opt)
+    # ----------------------------------------
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--opt', type=str, default=json_path, help='Path to option JSON file.')
+    parser.add_argument('--launcher', default='pytorch', help='job launcher')
+    parser.add_argument('--local_rank', type=int, default=0)
+    parser.add_argument('--dist', default=False)
+
+    opt = option.parse(parser.parse_args().opt, is_train=True)
+
+    # ----------------------------------------
+    # distributed settings of training 
+    # ----------------------------------------
+    opt['dist'] = parser.parse_args().dist
+    if opt['dist']:
+        init_dist('pytorch')
+    opt['rank'], opt['word_size'] = get_dist_info()
+
+    if opt['rank'] == 0:
+        util.mkdirs((path for key, path in opt['path'].items() if 'pretrained' not in key))
+    
+    # ----------------------------------------
+    # update opt
+    # ----------------------------------------
+    # -->-->-->-->-->-->-->-->-->-->-->-->-->-
+    init_iter, init_path_G = option.find_last_checkpoint(opt['path']['models'], net_type='G')
+    opt['path']['pretrained_netG'] = init_path_G
+    current_step = init_iter
+    # --<--<--<--<--<--<--<--<--<--<--<--<--<-
+
+    # ----------------------------------------
+    # save opt to  a '../option.json' file
+    # ----------------------------------------
+    if opt['rank'] == 0:
+        option.save(opt)
+    
+    # ----------------------------------------
+    # return None for missing key
+    # ----------------------------------------
+    opt = option.dict_to_nonedict(opt)
+
+    # ----------------------------------------
+    # configure logger
+    # ----------------------------------------
+    logger_name = 'train'
+    utils_logger.logger_info(logger_name, os.path.join(opt['path']['log'], logger_name+'.log'))
+    logger = logging.getLogger(logger_name)
+    logger.info(option.dict2str(opt))
+
+    # ----------------------------------------
+    # seed
+    # ----------------------------------------
+    seed = opt['train']['manual_seed']
+    if seed is None:
+        seed = random.randint(1, 10000)
+    logger.info('Random seed: {}'.format(seed))
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    '''
+    # ----------------------------------------
+    # Step--2 (creat dataloader)
+    # ----------------------------------------
+    '''
+
+    # ----------------------------------------
+    # 1) create_dataset
+    # 2) creat_dataloader for train and test
+    # ----------------------------------------
+    for phase, dataset_opt in opt['datasets'].items():
+        if phase == 'train':
+            train_set = define_Dataset(dataset_opt)
+            train_size = int(math.ceil(len(train_set) / dataset_opt['dataloader_batch_size']))
+            if opt['rank'] == 0:
+                logger.info('Number of train images: {:,d}, iters: {:,d}'.format(len(train_set), train_size))
+            if opt['dist']:
+                train_sampler = DistributedSampler(train_set, shuffle=dataset_opt['dataloader_shuffle'], drop_last=True, seed=seed)
+                train_loader = DataLoader(train_set,
+                                          batch_size=dataset_opt['dataloader_batch_size']//opt['num_gpu'],
+                                          shuffle=False,
+                                          num_workers=dataset_opt['dataloader_num_workers']//opt['num_gpu'],
+                                          drop_last=True,
+                                          pin_memory=True,
+                                          sampler=train_sampler)
+            else:
+                train_loader = DataLoader(train_set,
+                                          batch_size=dataset_opt['dataloader_batch_size'],
+                                          shuffle=dataset_opt['dataloader_shuffle'],
+                                          num_workers=dataset_opt['dataloader_num_workers'],
+                                          drop_last=True,
+                                          pin_memory=True)
+
+        elif phase == 'test':
+            test_set = define_Dataset(dataset_opt)
+            test_loader = DataLoader(test_set, batch_size=1,
+                                     shuffle=False, num_workers=1,
+                                     drop_last=False, pin_memory=True)
+        else:
+            raise NotImplementedError("Phase [%s] is not recognized." % phase)
+
+    '''
+    # ----------------------------------------
+    # Step--3 (initialize model)
+    # ----------------------------------------
+    '''
+
+    '''
+    # ----------------------------------------
+    # Step--4 (main training)
+    # ----------------------------------------
+    '''
+    for epoch in range(10000000000000): # don't forget the terminate condition
+        if opt['dist']:
+            train_sampler.set_epoch(epoch) # set the sampler in data distribution
+
+        for i, train_data in enumerate(train_loader):
+
+            #TODO: check the I/O of data
+            img_L = train_data['L']
+            img_H = train_data['H']
+            img_L = util.tensor2uint(img_L)
+            img_H = util.tensor2uint(img_H)
+            util.imsave(img_L, 'test_L.png')
+            util.imsave(img_H, 'test_H.png')
+            print(train_data['L_path'], train_data['H_path'])
+            time.sleep(10)
+
+if __name__ == '__main__':
+    main()
